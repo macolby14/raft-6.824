b1a6c4bececa76e3161b4cb4aa0cd253bc3577ed
Mark Colby <macolby14@gmail.com>
2022-06-05 15:48:57 -0400

    remove persist

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -25,9 +25,6 @@ import (
 	"sync/atomic"
 	"time"
 
-	"bytes"
-
-	"raft-6.824/labgob"
 	"raft-6.824/labrpc"
 )
 
@@ -162,42 +159,42 @@ func (rf *Raft) GetState() (int, bool) {
 func (rf *Raft) persist() {
 	// Your code here (2C).
 	// Example:
-	w := new(bytes.Buffer)
-	e := labgob.NewEncoder(w)
-	rf.mu.Lock()
-	e.Encode(rf.currentTerm)
-	e.Encode(rf.votedFor)
-	e.Encode(rf.log)
-	rf.mu.Unlock()
-	data := w.Bytes()
-	rf.persister.SaveRaftState(data)
+	// w := new(bytes.Buffer)
+	// e := labgob.NewEncoder(w)
+	// rf.mu.Lock()
+	// e.Encode(rf.currentTerm)
+	// e.Encode(rf.votedFor)
+	// e.Encode(rf.log)
+	// rf.mu.Unlock()
+	// data := w.Bytes()
+	// rf.persister.SaveRaftState(data)
 }
 
 //
 // restore previously persisted state.
 //
 func (rf *Raft) readPersist(data []byte) {
-	if data == nil || len(data) < 1 { // bootstrap without any state?
-		return
-	}
-	// Your code here (2C).
-	// Example:
-	r := bytes.NewBuffer(data)
-	d := labgob.NewDecoder(r)
-	var currentTerm int
-	var votedFor int
-	var log []Log
-	if d.Decode(&currentTerm) != nil ||
-		d.Decode(&votedFor) != nil ||
-		d.Decode(&log) != nil {
-		panic("Error decoding in readPersist")
-	} else {
-		rf.mu.Lock()
-		rf.currentTerm = currentTerm
-		rf.votedFor = votedFor
-		rf.log = log
-		rf.mu.Unlock()
-	}
+	// if data == nil || len(data) < 1 { // bootstrap without any state?
+	// 	return
+	// }
+	// // Your code here (2C).
+	// // Example:
+	// r := bytes.NewBuffer(data)
+	// d := labgob.NewDecoder(r)
+	// var currentTerm int
+	// var votedFor int
+	// var log []Log
+	// if d.Decode(&currentTerm) != nil ||
+	// 	d.Decode(&votedFor) != nil ||
+	// 	d.Decode(&log) != nil {
+	// 	panic("Error decoding in readPersist")
+	// } else {
+	// 	rf.mu.Lock()
+	// 	rf.currentTerm = currentTerm
+	// 	rf.votedFor = votedFor
+	// 	rf.log = log
+	// 	rf.mu.Unlock()
+	// }
 }
 
 //
